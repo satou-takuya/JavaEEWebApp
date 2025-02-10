d95d8145c4d968f82443fba25e5dc0139df011f8
Takuya Satou <ts23304010@ga.ttc.ac.jp>
2025-02-10 14:04:59 +0900

    InitialCommit

---
+++ JavaEEWebApp/src/main/java/jp/ac/ttc/webapp/HelloServlet.java
@@ -0,0 +1,29 @@
+package jp.ac.ttc.webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+    public HelloServlet() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.write("<h1>Hello,world</h1>");
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
